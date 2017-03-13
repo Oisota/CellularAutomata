b64509ef2e71ed6072925a643fb44afc9da24be8
Derek Morey <dman6505@gmail.com>
2017-03-13 00:01:00 -0400

    general update

--- automata.js
+++ automata.js
@@ -1,93 +1,67 @@
 'use strict';
 
-const Automata = (() => {
+function Automata(spec) {
 
-    function genFirstRow(cellsPerRow, rand, color1, color2) {
-        const randomSelect = (arr) => arr[Math.floor(Math.random() * arr.length)];
-        const row = document.createElement('div');
-        row.setAttribute('class', 'row');
-    
-        for (let i=0; i<cellsPerRow; i++) {
-            const cell = document.createElement('div');
-            cell.style.backgroundColor = rand ? randomSelect([color1, color2]) : color2;
-            row.appendChild(cell);
-        }
+	const initRow = (cellsPerRow, rand, colors) => {
+		const randChoice = arr => arr[Math.floor(Math.random() * arr.length)];
+		const row = document.createElement('div');
+		row.setAttribute('class', 'row');
 
-        if (!rand) {
-            row.children[Math.floor(cellsPerRow/2)].style.backgroundColor = color1;
-        }
-        return row; 
-    }
-    
-    function calcNewRow(rules, oldRow, color1, color2) {
-        const state = (cell) => cell.style.backgroundColor === color1;
-        const newRow = document.createElement('div');
-        newRow.setAttribute('class', 'row');
-    
-        const len = oldRow.children.length;
-        for (let i=0; i<len; i++) {
-            const left = oldRow.children[i-1] || oldRow.children[len-1];
-            const center = oldRow.children[i];
-            const right = oldRow.children[i+1] || oldRow.children[0];
-            const newCenter = document.createElement('div');
-    
-            rules.forEach((rule) => {
-                if (state(left) === rule.left && 
-                    state(center) === rule.center &&
-                    state(right) === rule.right) {
-                    newCenter.style.backgroundColor =  rule.newCell ? color1 : color2;
-                }
-            });
-            newRow.appendChild(newCenter);
-        }
-        return newRow;
-    }
+		Array.from(Array(cellsPerRow)).forEach(() => {
+			const cell = document.createElement('div');
+			cell.style.backgroundColor = rand ? randChoice(colors) : colors[1];
+			row.appendChild(cell);
+		});
 
-    function Color(hexCode) {
-        const el = document.createElement('div');
-        el.style.backgroundColor = hexCode;
-        return el.style.backgroundColor;
-    }
-    
-    function Rule(rule) {
-        return {
-            left: rule[0] === 1,
-            center: rule[1] === 1,
-            right: rule[2] === 1,
-            newCell: rule[3] === 1
-        };
-    }
-    
-    function createAutomata(spec) {
-        const exports = {};
-        const ruleBase = [
-            [1,1,1],
-            [1,1,0],
-            [1,0,1],
-            [1,0,0],
-            [0,1,1],
-            [0,1,0],
-            [0,0,1],
-            [0,0,0]
-        ];
-        const color1 = Color(spec.color1);
-        const color2 = Color(spec.color2);
-        const container = document.getElementById(spec.containerID);
-        container.innerHTML = '';
-        const cellsPerRow = Math.floor(container.offsetWidth / 10);
-        const rules = ruleBase
-            .map((rule, idx) => rule.concat(spec.rule[idx])) //add rule to rule base
-            .map(rule => Rule(rule)); //convert to rule objects
-        const newRow = calcNewRow.bind(undefined, rules);
-    
-        exports.run = () => {
-            container.appendChild(genFirstRow(cellsPerRow, spec.randState, color1, color2));
-            for (let i=0; i<spec.rowCount; i++) {
-                container.appendChild(newRow(container.children[i], color1, color2));
-            }
-        };
-        return exports;
-    }
-    
-    return createAutomata;
-})();
+		if (!rand) {
+			row.children[Math.floor(cellsPerRow/2)].style.backgroundColor = colors[0];
+		}
+		return row; 
+	};
+
+	const newRow = (rules, colors, oldRow) => {
+		const state = cell => cell.style.backgroundColor === colors[0];
+		const match = (rule, left, right, center) => { 
+			return state(left) === rule.left && state(center) === rule.center && state(right) === rule.right;
+		};
+		const rowDiv = document.createElement('div');
+		rowDiv.setAttribute('class', 'row');
+		Array.from(oldRow.children).forEach((el, idx, row) => {
+			const left = row[idx-1] || row[row.length-1];
+			const center = el;
+			const right = row[idx+1] || row[0];
+			const newCenter = document.createElement('div');
+			rules.forEach(rule => {
+				if (match(rule, left, right, center)) {
+					newCenter.style.backgroundColor = rule.cell ? colors[0] : colors[1];
+				}
+			});
+			rowDiv.appendChild(newCenter);
+		});
+		return rowDiv
+	};
+
+	const colors = spec.colors.map(hex => { //map hex code to color
+		const el = document.createElement('div');
+		el.style.backgroundColor = hex;
+		return el.style.backgroundColor;
+	});
+	const container = document.getElementById(spec.containerID);
+	const cellsPerRow = Math.floor(container.offsetWidth / 10);
+	const rules = Array.from(Array(8)) //create empty array
+		.map((_, idx) => ('000' + idx.toString(2)).slice(-3).split('').map(Number)) //convert to 0 - 7 bit array
+		.reverse()
+		.map((rule, idx) => rule.concat(spec.rule[idx])) //add rule to rule base
+		.map(rule => ['left','center','right','cell'].reduce((prev, cur, idx) => {
+			prev[cur] = rule[idx] === 1;
+			return prev;
+		}, {})); //convert to rule objects
+
+	container.innerHTML = '';
+	container.appendChild(initRow(cellsPerRow, spec.randState, colors));
+	Array.from(Array(spec.rowCount)).forEach((_, idx) => {
+		const row = newRow(rules, colors, container.children[idx])
+			container.appendChild(row);
+	});
+}
+
--- main.js
+++ main.js
@@ -1,40 +1,23 @@
 (() => {
-
-    const automata = Automata({
-        color1: '#661111',
-        color2: '#119999',
-        randState: true,
-        rowCount: 300,
-        containerID: 'container',
-        rule: [1,0,1,0,0,0,0,1]
-    });
-    automata.run();
-
+	Automata({
+		colors: ['#661111', '#119999'],
+		randState: true,
+		rowCount: 300,
+		containerID: 'container',
+		rule: [1,0,1,0,0,0,0,1]
+	});
 })();
 
-document
-.forms['regen-form']
-.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const color1 = event
-        .target[0]
-        .value,
-    color2 = event
-        .target[1]
-        .value,
-    rule = event
-        .target[2]
-        .value
-        .split('')
-        .map(value => Number(value)),
-    randState = event.target[3].checked,
-    automata = Automata({
-        color1: color1,
-        color2: color2,
-        randState: randState,
-        rowCount: 300,
-        containerID: 'container',
-        rule: rule
-    });
-    automata.run();
+document.forms['regen-form'].addEventListener('submit', (event) => {
+	event.preventDefault();
+	Automata({
+		colors: [
+			event.target['color1'].value,
+			event.target['color2'].value
+		],
+		randState: event.target['rand'].checked,
+		rule: event.target['rule'].value.split('').map(Number),
+		rowCount: 300,
+		containerID: 'container'
+	});
 });
