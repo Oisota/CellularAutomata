ad1d1e96ea50b3a35f3480816ef084e4d8e8a2cf
Derek Morey <derek.o.morey@gmail.com>
2019-02-12 22:48:35 -0500

    Initial row generation kinda works

--- src/components/cell-grid/main.js
+++ src/components/cell-grid/main.js
@@ -27,6 +27,30 @@ export default {
 	},
 	computed: {
 		cells() {
+			const row = this.initRow(100, this.rand, [this.color1, this.color2])
+				.map(color => `background-color: ${color}`);
+			return [row];
+		},
+		ruleArray() {
+			return this.rule.split('').map(Number);
 		}
-	}
+	},
+	methods: {
+		/**
+		 * @param {int} cellsPerRow - number of cells in each row
+		 * @param {bool} rand - indicate if row should be random or not
+		 * @param {Array<string>} colors - an array containing 2 color hexcodes as strings
+		 * @returns {div} the initial row div object
+		 */
+		initRow(cellsPerRow, rand, colors) {
+			const randChoice = arr => arr[Math.floor(Math.random() * arr.length)];
+			const row = Array.from(Array(cellsPerRow))
+				.map(() => rand ? randChoice(colors) : colors[1]);
+
+			if (!rand) {
+				row[Math.floor(cellsPerRow/2)] = colors[0];
+			}
+			return row; 
+		},
+	},
 };
