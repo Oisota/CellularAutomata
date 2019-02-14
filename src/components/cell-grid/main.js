179f32acd202ca2ff391766bc9f2db3e8e66f668
Derek Morey <derek.o.morey@gmail.com>
2019-02-13 23:00:07 -0500

    It sorta works now

--- src/components/cell-grid/main.js
+++ src/components/cell-grid/main.js
@@ -27,9 +27,28 @@ export default {
 	},
 	computed: {
 		cells() {
-			const row = this.initRow(100, this.rand, [this.color1, this.color2])
-				.map(color => `background-color: ${color}`);
-			return [row];
+			const rules = Array.from(Array(8)) //create empty array
+				.map((_, idx) => ('000' + idx.toString(2)).slice(-3).split('').map(Number)) //convert to 0 - 7 bit array
+				.reverse()
+				.map((rule, idx) => rule.concat(this.ruleArray[idx])) //add rule to rule base
+				.map(rule => ['left','center','right','cell'].reduce((prev, cur, idx) => {
+					prev[cur] = rule[idx] === 1;
+					return prev;
+				}, {})); //convert to rule objects
+			const row = this.initRow(100, this.rand, [this.color1, this.color2]);
+			/*
+			return Array.from(Array(this.rows)).reduce((acc, cur, idx) => {
+				acc.push(this.newRow(rules, acc[idx], [this.color1, this.color2]));
+				return acc;
+			}, [row]);
+			*/
+			const rows = [row];
+			Array.from(Array(this.rows)).forEach((_, idx) => {
+				const r = this.newRow(rules, [this.color1, this.color2], rows[idx]);
+				rows.push(r);
+			});
+			return rows
+				.map(r => r.map(c => `background-color: ${c}`));
 		},
 		ruleArray() {
 			return this.rule.split('').map(Number);
@@ -51,6 +70,31 @@ export default {
 				row[Math.floor(cellsPerRow/2)] = colors[0];
 			}
 			return row; 
+		},
+		/**
+		 * @param {} rules - 
+		 * @param {Array<string>} colors - an array containing 2 color hexcodes as strings
+		 * @param {div} oldRow - 
+		 */
+		newRow(rules, colors, oldRow) {
+			const state = cell => cell === colors[0];
+			const match = (rule, left, right, center) => { 
+				return state(left) === rule.left &&
+					state(center) === rule.center &&
+					state(right) === rule.right;
+			};
+			return oldRow.map((el, idx, row) => {
+				const left = row[idx-1] || row[row.length-1];
+				const center = el;
+				const right = row[idx+1] || row[0];
+				let newCenter = '';
+				rules.forEach(rule => { // use array.find here?
+					if (match(rule, left, right, center)) {
+						newCenter = rule.cell ? colors[0] : colors[1];
+					}
+				});
+				return newCenter;
+			});
 		},
 	},
 };
