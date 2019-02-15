84c8bb3751ddba47a5a4e5cf3417b97fb9037259
Derek Morey <derek.o.morey@gmail.com>
2019-02-14 19:29:06 -0500

    Added debounce to color input changes
    This was done so that the page won't freeze when the color is changed
    rapidle.

--- src/components/app/main.js
+++ src/components/app/main.js
@@ -1,4 +1,5 @@
 import CellGrid from '../cell-grid';
+import debounce from 'lodash/debounce';
 
 export default {
 	name: 'app',
@@ -9,14 +10,36 @@ export default {
 		return {
 			color1: '#661111',
 			color2: '#119999',
+			debouncedColor1: '#661111',
+			debouncedColor2: '#119999',
 			rand: false,
 			rule: [1,0,0,0,0,0,0,1].map(x => x ? {value: true} : {value: false}),
 			rows: 300,
 		};
 	},
+	created() {
+		this.setColor1 = debounce(this.setColor1, 250);
+		this.setColor2 = debounce(this.setColor2, 250);
+	},
 	computed: {
 		ruleString() {
 			return this.rule.map(r => r.value ? 1 : 0).join('');
 		}
+	},
+	methods: {
+		setColor1(c) {
+			this.color1 = c;
+		},
+		setColor2(c) {
+			this.color2 = c;
+		},
+	},
+	watch: {
+		debouncedColor1(newColor) {
+			this.setColor1(newColor);
+		},
+		debouncedColor2(newColor) {
+			this.setColor2(newColor);
+		},
 	}
 };
